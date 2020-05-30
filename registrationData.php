160513b1c2289f08dddadb5f5bb961e6bea15149
Dominik Bonna <bonna.dominik@gmail.com>
2020-05-30 15:33:42 +0200

    functionality ended: data display

--- registrationData.php
+++ registrationData.php
@@ -5,17 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rejestracja</title>
     <link rel="stylesheet" href="/styles/registrationData.css">
-    <style>
-        main{
-            height:100vh;
-        }
-    </style>
 </head>
 <body>
     <?php
 
     if (isset($_POST['submit'])) {
-        if (empty($_POST['user-name'])||empty($_POST['user-last-name'])||empty($_POST['user-email'])) {
+        if (empty($_POST['user-name'])||empty($_POST['user-last-name'])||empty($_POST['user-email'])||empty($_POST['user-sex'])||empty($_POST['user-password'])) {
             {
                 echo 
                 '<header>
@@ -23,8 +18,12 @@
                     <p>Wystąpił błąd podczas rejestracji</p>
                 </header>
                 <main>
-                    <p>Musisz wypełnić wszystkie pola formularza.</p>
-                    Powrót do formularza: <a href="index.html">Klik</a>
+                    <ul>
+                        <li>Musisz wypełnić wszystkie pola formularza.</li>
+                        <li>
+                            <a href="index.html">Powrót do formularza</a>
+                        </li>
+                    </ul>
                 </main>';
             }
         }
@@ -43,24 +42,17 @@
             <main>
                 <ul>
                     <li>
-                        Imię: $firstName
+                        Imię<br> $firstName
                     </li>
                     <li>
-                        Nazwisko: $lastName
+                        Nazwisko<br> $lastName
                     </li>
                     <li>
-                        Płeć: $sex
+                        Płeć<br> $sex
                     </li>
                     <li>
-                        E-mail: $eMail
+                        E-mail<br> $eMail
                     </li>";
-            
-        //     if (is_uploaded_file($_FILES['UPLOADED_FILE']['tmp_name'])) {
-        //         echo "Plik został odebrany";
-        //     } else {
-        //         echo "Plik nie został odebrany";
-        //     }
-        // }
             
             $namesOfExistingFiles = array(0);
             if (is_uploaded_file($_FILES['UPLOADED_FILE']['tmp_name'])) {
@@ -69,28 +61,91 @@
                 $extension = pathinfo($_FILES['UPLOADED_FILE']['name'], PATHINFO_EXTENSION);
         
                 if (!in_array($extension, $allowedExtensions)) {
-                    echo "<li>Wprowadziłeś plik z niepoprawnym rozszerzeniem. Spróbuj ponownie.</li>";
+                    echo 
+                    '<li>
+                        Wprowadziłeś plik z niepoprawnym rozszerzeniem
+                    </li>
+                    <li>
+                    <a href="aboutMe.html">Przejdź do strony o mnie</a>
+                    </li>
+                        <span>lub</span>
+                    <li>
+                        <a href="index.html">Powrót do formularza</a>
+                    </li>';
                 } else {
                     switch ($_FILES['UPLOADED_FILE']['error']) {
                         case 0:
-                            echo "<li>Zdjęcie zostało przesłane prawidłowo.</li>";
+
+                            $newFileName = max($namesOfExistingFiles)+1;
+                            $_FILES['UPLOADED_FILE']['name'] = $newFileName.".$extension";
+                            move_uploaded_file($_FILES['UPLOADED_FILE']['tmp_name'],__DIR__.$path.$_FILES['UPLOADED_FILE']['name']);
+
+                            echo 
+                                '<li>
+                                    Zdjęcie profilowe<br>';
+                            echo 
+                                '<div style="
+                                background-image: url(uploads/'.$newFileName.'.'.$extension.');
+                                width: 200px;
+                                height: 200px;
+                                background-position: center; background-size: cover;
+                                margin-top: 10px;
+                                margin-bottom: 10px;"></div>';
+                            
+                            $fileSizeInMegabytes = round($_FILES['UPLOADED_FILE']['size'] / 1048576,2);
+                            echo 'Nazwa pliku: '.$_FILES['UPLOADED_FILE']['name'].'<br>';
+                            echo 'Typ pliku: '.$_FILES['UPLOADED_FILE']['type'].'<br>';
+                            echo 'Rozmiar pliku: '.$fileSizeInMegabytes.' MB</li>';
+
+                            echo 
+                            '<li>
+                                <a href="aboutMe.html">Przejdź do strony o mnie</a>
+                            </li>';
+                            
                             break;
                         
                         case 2:
-                            echo "<li>Rozmiar pliku jest zbyt duży.</li>";
+                            echo 
+                                '<li>
+                                    Rozmiar pliku jest zbyt duży
+                                </li>
+                                <li>
+                                    <a href="aboutMe.html">Przejdź do strony o mnie</a>
+                                </li>
+                                <span>lub</span>
+                                <li>
+                                    <a href="index.html">Powrót do formularza</a>
+                                </li>';
                             break;
         
                         case 4:
-                            echo "<li>Plik nie został wysłany</li>"; 
+                            echo
+                                '<li>
+                                    Plik nie został wysłany
+                                </li>
+                                <li>
+                                <a href="aboutMe.html">Przejdź do strony o mnie</a>
+                                </li>
+                                    <span>lub</span>
+                                <li>
+                                    <a href="index.html">Powrót do formularza</a>
+                                </li>'; 
                             break;
                         
                         default:
-                            echo "<li>Wystąpił błąd podczas wysyłania pliku na serwer.</li>";
+                            echo 
+                                '<li>
+                                    Wystąpił błąd podczas wysyłania pliku na serwer
+                                </li>
+                                <li>
+                                    <a href="aboutMe.html">Przejdź do strony o mnie</a>
+                                </li>
+                                    <span>lub</span>
+                                <li>
+                                    <a href="index.html">Powrót do formularza</a>
+                                </li>';
                             break;
                     }
-                    $newFileName = max($namesOfExistingFiles)+1;
-                    $_FILES['UPLOADED_FILE']['name'] = $newFileName.".$extension";
-                    move_uploaded_file($_FILES['UPLOADED_FILE']['tmp_name'],__DIR__.$path.$_FILES['UPLOADED_FILE']['name']);
                 }
             }
         }
